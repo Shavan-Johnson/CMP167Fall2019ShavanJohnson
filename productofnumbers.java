a3819717352d2ae3f8ec880b746873ffcd5c2f2b
Shavan-Johnson <54905526+Shavan-Johnson@users.noreply.github.com>
2019-09-09 12:03:23 -0400

    Add files via upload

---
+++ productofnumbers.java
@@ -0,0 +1,20 @@
+package weektwopart2;
+
+import java.util.Scanner;
+
+public class productofnumbers {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter four numbers to multiply");
+		
+		int num1 = input.nextInt();
+		int num2 = input.nextInt();
+		int num3 = input.nextInt();
+		int num4 = input.nextInt();
+		
+		int result = num1 * num2 * num3 * num4;
+		System.out.println(result);
+		
+	}
+
+}
---
+++ sumofnumbers.java
@@ -0,0 +1,17 @@
+package weektwopart2;
+import java.util.Scanner;
+public class sumofnumbers {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter three values to add");
+	
+	int num1 = input.nextInt();
+	int num2 = input.nextInt();
+	int num3 = input.nextInt();
+	
+	int result = num1 + num2 + num3;
+	System.out.println(result);
+	
+
+}
+}
