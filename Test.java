01cb322b0f6e964106338006e78a1e7e88d8350c
Shavan-Johnson <54905526+Shavan-Johnson@users.noreply.github.com>
2019-10-07 12:40:17 -0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,20 @@
+package week6;
+
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args){
+		
+		int n = 40;
+		int x = 12;
+		int z = 20;
+		int result = n * x * z;
+		
+		System.out.println("The Product is");
+		System.out.println(result);
+		
+		
+	}
+	
+
+}
