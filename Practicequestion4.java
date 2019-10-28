db2329349142b54c2d57a29b5c63a9f07081d116
Shavan-Johnson <54905526+Shavan-Johnson@users.noreply.github.com>
2019-10-28 12:02:02 -0400

    Add files via upload

---
+++ Practicequestion4.java
@@ -0,0 +1,30 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Practicequestion4 {
+	public static void main(String[] args) {
+		
+		String name1;
+		String petName;
+		
+		Scanner name = new Scanner(System.in);
+		
+		System.out.println("Enter your name:");
+		name1 = name.next();
+		
+		System.out.println("Enter your pet's name:");
+		petName = name.next();
+		
+	
+		
+		System.out.printf("%s, has %d characters.",name1, petName.length());
+	
+	}
+
+	
+		
+	}
+
+	
+
+
